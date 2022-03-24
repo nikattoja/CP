f0eb59ec85ff58e919e4bc1995b270c83657e4dc
236680 Tutkaj Weronika <236680@edu.p.lodz.pl>
2022-03-24 20:54:55 +0100

    Dodanie testow jednostkowych

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -2,6 +2,21 @@
 {
     public class Class1
     {
+        private double a, b;
+        public double Sum()
+        {
+            return a + b;
+        }
 
+        public double Multiply()
+        {
+            return a * b;
+        }
+
+        public Class1(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
     }
 }
---
+++ ClassLibrary1/ClassLibrary1Tests/Class1Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        Class1 class1 = new(1.2, 3.6);
+
+        [TestMethod()]
+        public void SumTest()
+        {
+            Assert.Equals(4.8, class1.Sum());
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Assert.Equals(4.32, class1.Multiply());
+        }
+    }
+}
