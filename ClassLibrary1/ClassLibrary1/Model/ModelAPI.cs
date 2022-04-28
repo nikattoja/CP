9a945a5d07a666c7d9d5f90784ebc83e5743f7ee
Weronika Tutkaj <236680@edu.p.lodz.pl>
2022-04-28 20:43:59 +0200

    Model changes

---
+++ ClassLibrary1/ClassLibrary1/Model/ModelAPI.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Numerics;
+using TPW.Logika;
+
+namespace TPW.Presentation.Model
+{
+    public class OnPositionChangeUiAdapterEventArgs : EventArgs
+    {
+        public readonly Vector2 Position;
+        public readonly int Id;
+
+        public OnPositionChangeUiAdapterEventArgs(Vector2 position, int id)
+        {
+            this.Position = position;
+            Id = id;
+        }
+    }
+    public abstract class ModelAPI
+    {
+        public abstract void StartSimulation();
+        public abstract void StopSimulation();
+        public abstract void SetBallNumber(int amount);
+        public abstract int GetBallsCount();
+        public abstract void OnBallPositionChange(OnPositionChangeUiAdapterEventArgs args);
+        public abstract event EventHandler<OnPositionChangeUiAdapterEventArgs> BallPositionChange;
+
+        public static ModelAPI CreateApi()
+        {
+            return new MainModel();
+        }
+        internal class MainModel : ModelAPI
+        {
+
+            private readonly Vector2 boardSize;
+            private int ballsAmount;
+            private LogicApi ballsLogic;
+
+            public override event EventHandler<OnPositionChangeUiAdapterEventArgs> BallPositionChange;
+
+            public MainModel()
+            {
+                boardSize = new Vector2(650, 400);
+                ballsAmount = 0;
+                ballsLogic = LogicApi.CreateBallsLogic(boardSize);
+                ballsLogic.PositionChange += (sender, args) =>
+                {
+                    BallPositionChange?.Invoke(this, new OnPositionChangeUiAdapterEventArgs(args.Ball.Position, args.Ball.Id));
+                };
+            }
+            public override void StartSimulation()
+            {
+                ballsLogic.AddBalls(ballsAmount);
+                ballsLogic.StartSimulation();
+            }
+
+            public override void StopSimulation()
+            {
+                ballsLogic.StopSimulation();
+                ballsLogic = LogicApi.CreateBallsLogic(boardSize);
+                ballsLogic.PositionChange += (sender, args) =>
+                {
+                    BallPositionChange?.Invoke(this, new OnPositionChangeUiAdapterEventArgs(args.Ball.Position, args.Ball.Id));
+                };
+            }
+
+            public override void SetBallNumber(int amount)
+            {
+                ballsAmount = amount;
+            }
+
+            public override int GetBallsCount()
+            {
+                return ballsAmount;
+            }
+
+            public override void OnBallPositionChange(OnPositionChangeUiAdapterEventArgs args)
+            {
+                BallPositionChange?.Invoke(this, args);
+            }
+        }
+    }
+}
