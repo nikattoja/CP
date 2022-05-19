5b8c17f617b9aded286402fdfae0a685fbe915ff
Weronika Tutkaj <236680@edu.p.lodz.pl>
2022-05-19 21:12:31 +0200

    modelapi changes

--- ClassLibrary1/ClassLibrary1/Model/ModelAPI.cs
+++ ClassLibrary1/ClassLibrary1/Model/ModelAPI.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Numerics;
+using System.Reactive;
+using System.Reactive.Linq;
 using TPW.Logika;
+using TPW.Dane;
 
 namespace TPW.Presentation.Model
 {
-    public class OnPositionChangeUiAdapterEventArgs : EventArgs
+    public class OnPositionChange : EventArgs
     {
         public readonly Vector2 Position;
         public readonly int Id;
-
-        public OnPositionChangeUiAdapterEventArgs(Vector2 position, int id)
+        public OnPositionChange(Vector2 position, int id)
         {
             this.Position = position;
             Id = id;
@@ -18,24 +21,25 @@ namespace TPW.Presentation.Model
     public abstract class ModelAPI
     {
         public abstract void StartSimulation();
-        public abstract void StopSimulation();
+       // public abstract void StopSimulation();
         public abstract void SetBallNumber(int amount);
         public abstract int GetBallsCount();
-        public abstract void OnBallPositionChange(OnPositionChangeUiAdapterEventArgs args);
-        public abstract event EventHandler<OnPositionChangeUiAdapterEventArgs> BallPositionChange;
+        public abstract void OnBallPositionChange(OnPositionChange args);
+        public abstract event EventHandler<OnPositionChange> BallPositionChange;
 
         public static ModelAPI CreateApi()
         {
             return new MainModel();
         }
+
         internal class MainModel : ModelAPI
         {
 
             private readonly Vector2 boardSize;
             private int ballsAmount;
             private LogicApi ballsLogic;
-
-            public override event EventHandler<OnPositionChangeUiAdapterEventArgs> BallPositionChange;
+            private IObservable<EventPattern<OnPositionChange>> eventObservable = null;
+            public override event EventHandler<OnPositionChange> BallPositionChange;
 
             public MainModel()
             {
@@ -44,8 +48,9 @@ namespace TPW.Presentation.Model
                 ballsLogic = LogicApi.CreateBallsLogic(boardSize);
                 ballsLogic.PositionChange += (sender, args) =>
                 {
-                    BallPositionChange?.Invoke(this, new OnPositionChangeUiAdapterEventArgs(args.Ball.Position, args.Ball.Id));
+                    BallPositionChange?.Invoke(this, new OnPositionChange(args.Ball.Position, args.Ball.Id));
                 };
+                eventObservable = Observable.FromEventPattern<OnPositionChange>(this, "BallChanged");
             }
             public override void StartSimulation()
             {
@@ -53,7 +58,7 @@ namespace TPW.Presentation.Model
                 ballsLogic.StartSimulation();
             }
 
-            public override void StopSimulation()
+         /*   public override void StopSimulation()
             {
                 ballsLogic.StopSimulation();
                 ballsLogic = LogicApi.CreateBallsLogic(boardSize);
@@ -61,7 +66,7 @@ namespace TPW.Presentation.Model
                 {
                     BallPositionChange?.Invoke(this, new OnPositionChangeUiAdapterEventArgs(args.Ball.Position, args.Ball.Id));
                 };
-            }
+            }*/
 
             public override void SetBallNumber(int amount)
             {
@@ -73,7 +78,7 @@ namespace TPW.Presentation.Model
                 return ballsAmount;
             }
 
-            public override void OnBallPositionChange(OnPositionChangeUiAdapterEventArgs args)
+            public override void OnBallPositionChange(OnPositionChange args)
             {
                 BallPositionChange?.Invoke(this, args);
             }
